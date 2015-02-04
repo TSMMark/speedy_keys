cb09fd73bed49cdc9b18d7baab3b2d5af883383b
TSMMark <mark.allen@vydia.com>
2015-02-04 00:17:40 -0500

    use game route for root path for now

--- client/main.jsx
+++ client/main.jsx
@@ -14,18 +14,23 @@ Meteor.startup(function () {
 
       updateDeps();
 
-      if (currentUser) {
-        subRoutes = [
-          (<Route name="playGame" key="playGame" path="play/:gameId" handler={Views.PlayGame}/>),
-          (<DefaultRoute name="default" key="default" handler={Views.Home}/>)
-        ];
-      }
-      else if (missingServiceConfig()) {
-        subRoutes = (<DefaultRoute name="default" key="default" handler={Views.ConfigureServices}/>);
-      }
-      else {
-        subRoutes = (<DefaultRoute name="default" key="default" handler={Views.SignIn}/>);
-      }
+      subRoutes = [
+        (<Route name="playGame" key="playGame" path="play/:gameId" handler={Views.PlayGame}/>),
+        (<DefaultRoute name="default" key="default" handler={Views.PlayGame}/>)
+      ];
+
+      // if (currentUser) {
+      //   subRoutes = [
+      //     (<Route name="playGame" key="playGame" path="play/:gameId" handler={Views.PlayGame}/>),
+      //     (<DefaultRoute name="default" key="default" handler={Views.Home}/>)
+      //   ];
+      // }
+      // else if (missingServiceConfig()) {
+      //   subRoutes = (<DefaultRoute name="default" key="default" handler={Views.ConfigureServices}/>);
+      // }
+      // else {
+      //   subRoutes = (<DefaultRoute name="default" key="default" handler={Views.SignIn}/>);
+      // }
 
       routes = (
         <Route name="app" path="/" handler={Views.App}>
