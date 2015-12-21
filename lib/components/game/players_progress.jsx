7b2dbdd337d1bb1521a89c45c9d3c0e2ecfd1987
TSMMark <mark.allen@vydia.com>
2015-12-21 03:51:13 -0500

    fix player progress shown always

--- lib/components/game/players_progress.jsx
+++ lib/components/game/players_progress.jsx
@@ -13,41 +13,37 @@ Components.PlayersProgress = React.createClass({
     var dual = this.props.dual
       , playerEmoji = this.props.playerEmoji
       , opponentEmoji = this.props.opponentEmoji
-      , playerProgressBar
-      , opponentProgressBar
-      , playerProgressEmoji
-      , opponentProgressEmoji
+      , playerProgress
+      , opponentProgress
       , centerIndicator
       , modifier = 1.0;
 
     if (this.props.playerProgress >= 0) {
-      playerProgressBar = (
-        <Components.ProgressBar progress={this.props.playerProgress}
-                                mainColor="teal"
-                                backgroundColor="teal lighten-5"
-                                modifier={modifier} />
-      );
-
-      playerProgressEmoji = (
-        <div className="player-icon-container">
-          <Components.Emoji emoji={playerEmoji}
-            style={{ left: (this.props.playerProgress * 100) + "%" }} />
+      playerProgress = (
+        <div className="player-progress">
+          <Components.ProgressBar progress={this.props.playerProgress}
+            mainColor="teal"
+            backgroundColor="teal lighten-5"
+            modifier={modifier} />
+          <div className="player-icon-container">
+            <Components.Emoji emoji={playerEmoji}
+              style={{ left: (this.props.playerProgress * 100) + "%" }} />
+          </div>
         </div>
       );
     }
 
     if (this.props.opponentProgress >= 0) {
-      opponentProgressBar = (
-        <Components.ProgressBar progress={this.props.opponentProgress}
-                                mainColor="red"
-                                backgroundColor="red lighten-5"
-                                modifier={modifier} />
-      );
-
-      opponentProgressEmoji = (
-        <div className="opponent-icon-container">
-          <Components.Emoji emoji={opponentEmoji}
-            style={{ left: (this.props.opponentProgress * 100) + "%" }} />
+      opponentProgress = (
+        <div className="opponent-progress">
+          <Components.ProgressBar progress={this.props.opponentProgress}
+            mainColor="red"
+            backgroundColor="red lighten-5"
+            modifier={modifier} />
+          <div className="opponent-icon-container">
+            <Components.Emoji emoji={opponentEmoji}
+              style={{ left: (this.props.opponentProgress * 100) + "%" }} />
+          </div>
         </div>
       );
     }
@@ -58,14 +54,8 @@ Components.PlayersProgress = React.createClass({
 
     return (
       <div className={cx({ "players-progress": true, "dual-progress": dual })}>
-        <div className="player-progress">
-          {playerProgressBar}
-          {playerProgressEmoji}
-        </div>
-        <div className="opponent-progress">
-          {opponentProgressBar}
-          {opponentProgressEmoji}
-        </div>
+        {playerProgress}
+        {opponentProgress}
       </div>
     );
   }
